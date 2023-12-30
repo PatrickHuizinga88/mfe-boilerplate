273e8f71f30e4ad4125966eff288d6204954e96e
PatrickHuizinga88 <patrickhuizinga44@gmail.com>
2023-12-30 15:51:10 +0100

    Initial commit

---
+++ apps/host/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Dashboard</h1>
+    <div>
+        Host
+        <Counter></Counter>
+        <Remote></Remote> 
+    </div>
+
+    <Card title="test"/>
+</template>
+
+<script setup lang="ts">
+import { Card } from 'ui'
+</script>
---
+++ packages/ui/src/Card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h2>{{ props.title }}</h2>
+        <p><slot /></p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+}>();
+</script>
